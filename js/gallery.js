f5612e7cac61d8b09fff047f964cf143dca753d6
AnSeonJeong <asj081@naver.com>
2022-05-29 18:38:26 +0900

    gallery

---
+++ js/gallery.js
@@ -0,0 +1,31 @@
+const slide = document.querySelectorAll("#gallery .slide");
+const slideList1 = slide[0].querySelectorAll(".photo");
+const slideList2 = slide[1].querySelectorAll(".photo");
+
+function makeClone() {
+  for (let i = 0; i < slideList1.length; i++) {
+    const colonedSlide1 = slideList1[i].cloneNode(true);
+    colonedSlide1.className = "photo clone";
+    slide[0].appendChild(colonedSlide1);
+  }
+
+  for (let i = slideList2.length - 1; i >= 0; i--) {
+    const colonedSlide2 = slideList2[i].cloneNode(true);
+    colonedSlide2.className = "photo clone";
+    slide[1].prepend(colonedSlide2);
+  }
+}
+
+makeClone();
+
+const photos = document.querySelectorAll(".slide .photo");
+console.log(photos);
+
+const CLICKED = "clicked";
+
+photos.forEach((photo) => {
+  photo.addEventListener("click", () => {
+    photo.classList.toggle(CLICKED);
+    photo.querySelector("img").classList.toggle(CLICKED);
+  });
+});
